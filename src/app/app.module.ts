e5f9960cf6c8c839ea63ddad9d37405abd65309d
Michel SADEU <michel.sadeu-ngakou@neel.cnrs.fr>
2021-06-05 21:54:31 +0200

    Architecture des composants du projet, Création des différents composants, interfaces et services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './composants/menu/menu.component';
+import { ArticlesComponent } from './composants/articles/articles.component';
+import { ConnexionComponent } from './composants/auth/connexion/connexion.component';
+import { InscriptionComponent } from './composants/auth/inscription/inscription.component';
+import { CommandeComponent } from './composants/commande/commande.component';
+import { PanierComponent } from './composants/panier/panier.component';
+import { LivreUniqueComponent } from './composants/livre-unique/livre-unique.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ArticlesComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    CommandeComponent,
+    PanierComponent,
+    LivreUniqueComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/composants/livre-unique/livre-unique.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livre-unique',
+  templateUrl: './livre-unique.component.html',
+  styleUrls: ['./livre-unique.component.css']
+})
+export class LivreUniqueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
