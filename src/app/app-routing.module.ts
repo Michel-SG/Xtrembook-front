7410b87ede11ef2266f7eb668c84ea1e520ae4c2
manu <manu.v>
2021-06-05 17:49:02 +0200

    ajout composant panier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanierComponent } from './composants/panier/panier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "panier", component : PanierComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './composants/menu/menu.component';
+import { PanierComponent } from './composants/panier/panier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
