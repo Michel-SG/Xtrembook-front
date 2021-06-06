85006b71dd81474d3e0d0b98c1fd8ddd5e2a1eaa
Michel SADEU <michel.sadeu-ngakou@neel.cnrs.fr>
2021-06-06 19:30:11 +0200

    Ajoût de HttpClientModule et BehaviorSubject pour l'hauthentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './composants/menu/menu.component';
@@ -27,7 +28,8 @@ import { LivreUniqueComponent } from './composants/livre-unique/livre-unique.com
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from 'src/app/interfaces/user';
+// import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  isAuth$ = new BehaviorSubject<boolean>(false);
   constructor() { }
+
+  createUser = (user:User) => {
+    return new Promise((resolve, reject) => {
+      // let host = environment.host;
+      
+
+    })
+  }
 }
