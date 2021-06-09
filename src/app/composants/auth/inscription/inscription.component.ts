6ce79cd87761d98492bf505718bfb6e84d2bf7e3
Michel SADEU <michel.sadeu-ngakou@neel.cnrs.fr>
2021-06-09 08:11:46 +0200

    Ajoût des formControls dans le FormGroup

--- src/app/composants/auth/inscription/inscription.component.ts
+++ src/app/composants/auth/inscription/inscription.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -8,6 +8,21 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
+
+  signupForm = this.formBuilder.group({
+    nom: ['', Validators.required],
+    prenom: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    motDePasse: ['', Validators.required],
+    adresse: this.formBuilder.group({
+      numero: [null, Validators.required],
+      typeDeVoie: ['', Validators.required],
+      nomDeVoie: ['', Validators.required],
+      codePostal: ['', Validators.required],
+      ville: ['', Validators.required],
+      pays: ['', Validators.required]
+    })
+  });
 
   constructor(private formBuilder: FormBuilder,
     private authService: AuthService) { }
