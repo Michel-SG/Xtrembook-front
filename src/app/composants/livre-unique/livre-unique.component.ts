9f71e09bd7036d5660863319100970a3c63f2614
Scara84 <antho.scara@yahoo.fr>
2021-06-10 14:05:44 +0200

    page détails initialisation fonctionnel

--- src/app/composants/livre-unique/livre-unique.component.ts
+++ src/app/composants/livre-unique/livre-unique.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Livre } from 'src/app/interfaces/livre';
+import { ArticleService } from 'src/app/services/articles/article.service';
 
 @Component({
   selector: 'app-livre-unique',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./livre-unique.component.css']
 })
 export class LivreUniqueComponent implements OnInit {
+referenceArticle = "";
+livre: Livre = {};
 
-  constructor() { }
+  constructor(private articlesService: ArticleService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.initialize();
   }
-
+initialize() {
+  this.route.paramMap.subscribe(
+    (value) => {
+      this.referenceArticle = value.get('referenceArticle') ?? '';
+      console.log(this.referenceArticle)
+      this.articlesService.getOneById(this.referenceArticle).subscribe((res) => {
+        this.livre = res;
+        console.log(this.livre)
+      });
+    });
+  }
 }
--- src/app/interfaces/livre.ts
+++ src/app/interfaces/livre.ts
@@ -1,2 +1,9 @@
 export interface Livre {
+    referenceArticle?: number,
+    titre?: string,
+    resumed?: string,
+    prixUnit?: number,
+    stock?: number,
+    imageUrl?: string,
+    isbn13?: string
 }
