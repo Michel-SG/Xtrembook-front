14b0da91eed726a50134b63f5cdc59c342adb3ed
Michel SADEU <michel.sadeu-ngakou@neel.cnrs.fr>
2021-06-10 14:10:39 +0200

    Recupération du nom de l'utilisateur dans authService

--- src/app/composants/menu/menu.component.ts
+++ src/app/composants/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  prenom: string;
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.prenom = this.auth.getPrenom();
   }
 
 }
