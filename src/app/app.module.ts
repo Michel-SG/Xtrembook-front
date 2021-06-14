d8e173b98e5703ba73eb27c714a1ee25d611a9c6
manu <manu.v>
2021-06-14 12:39:50 +0200

    dernières modifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LivreUniqueComponent } from './composants/livre-unique/livre-unique.com
 import { CommandeValideeComponent } from './composants/commande-validee/commande-validee.component';
 import { RechercheComponent } from './composants/recherche/recherche.component';
 import { AdministrateurComponent } from './composants/administrateur/administrateur.component';
+import { FooterComponent } from './composants/footer/footer.component';
 
 
 
@@ -32,6 +33,7 @@ import { AdministrateurComponent } from './composants/administrateur/administrat
     CommandeValideeComponent,
     RechercheComponent,
     AdministrateurComponent,
+    FooterComponent,
    
    
    
